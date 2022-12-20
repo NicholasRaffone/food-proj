a1e6f9644da3019de9b1cf34ed217eb55c72f079
Nicholas Raffone <nicholas.raffone@gmail.com>
2022-12-19 22:39:55 -0500

    f

--- context/mapContext.js
+++ context/mapContext.js
@@ -12,42 +12,25 @@ export function MapWrapper({ children }) {
   const mapRef = useRef(null)
 
   useEffect(()=>{
+
+    const loadScript = (src) =>
+      new Promise((resolve, reject) => {
+        if (document.querySelector(`script[src="${src}"`)) return resolve()
+        const script = document.createElement('script')
+        script.src = src
+        script.onload = () => resolve()
+        script.onerror = (err) => reject(err)
+        document.body.appendChild(script)
+    })
+    const src = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyDQsWK5-SAeyflw7xXRIpL99XPgmGWGmbg&libraries=places'
+
     if(mapRef){
-      const map = new google.maps.Map(mapRef.current);
-    
-      const request = {
-        query: "Black owned west village",
-        type: ["restaurant"],
-      };
-      setPlaceService(new google.maps.places.PlacesService(map));
-      setAutoCService(new google.maps.places.AutocompleteService());
+      loadScript().then(()=>{
+        const map = new google.maps.Map(mapRef.current);
+        setPlaceService(new google.maps.places.PlacesService(map));
+        setAutoCService(new google.maps.places.AutocompleteService());
+      })
 
-    
-    //   const service = new google.maps.places.PlacesService(map);
-    
-    //   service.textSearch(request, (
-    //     results,
-    //     status
-    //   ) => {
-    //     if (status === google.maps.places.PlacesServiceStatus.OK && results) {
-    //       for (let i = 0; i < results.length; i++) {
-    //         console.log(results[i])
-    //       }
-    //     }
-    //   });
-      // const autoCompTypes = ["geocode"];
-      // const autoCompService = new google.maps.places.AutocompleteService();
-
-      // autoCompService.getPlacePredictions({ input: "west village", types: autoCompTypes }, (
-      //   results,
-      //   status
-      // ) => {
-      //   if (status === google.maps.places.PlacesServiceStatus.OK && results) {
-      //     for (let i = 0; i < results.length; i++) {
-      //       console.log(results[i])
-      //     }
-      //   }
-      // });
     }
   },[mapRef]);
   return (
